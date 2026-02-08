d2d9f766262d5818db7a8763a807ba2fe8b43b37
Richard Scott <bububooo3@gmail.com>
2026-02-08 14:10:05 -0500

    Updated StarterGui/DefaultUI.lua

---
+++ StarterGui/DefaultUI.lua
@@ -0,0 +1,8 @@
+-- @ScriptType: LocalScript
+local SG = game:GetService("StarterGui")
+
+SG:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+
+SG:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
+SG:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, true)
+SG:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, true)
