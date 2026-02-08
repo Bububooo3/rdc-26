5befcf855aebb4655286979db752294f866b1ae6
Richard Scott <bububooo3@gmail.com>
2026-02-07 20:53:21 -0500

    Updated ServerScriptService/Intermission.lua

--- ServerScriptService/Intermission.lua
+++ ServerScriptService/Intermission.lua
@@ -19,7 +19,7 @@ local num_round_spawns = #round_spawns
 local round_ongoing = false
 
 local RS = game:GetService("ReplicatedStorage")
-local fxns, events = RS:WaitForChild("Functions"), RS:WaitForChild("Events")
+local events = RS:WaitForChild("Events")
 local refreshTimer_event = events:WaitForChild("refreshTimer")
 local displayMsg_event = events:WaitForChild("displayMsg")
 
@@ -38,20 +38,20 @@ local function teleportPlrs() ----> Move all plrs based on whether the game's go
 	end
 end
 
-local function displayMsg(msg: string, t: number)
+local function displayMsg(msg: string, t: number, disable: boolean)
+	if not disable then disable = false end
 	if not msg then msg = "" end
 	t = math.max(0, t) ----> Msg will be "permanent" until a new msg with a set time = t replaces it
 
-	pcall(displayMsg_event.FireAllClients, displayMsg_event, msg, t) ----> Display new message on client-side
+	pcall(displayMsg_event.FireAllClients, displayMsg_event, msg, t, disable) ----> Display new message on client-side
 
-	return {Wait = function()
+	return {Wait = function() ----> We can use wait if we wanna wait for the msg to leave before moving on
 		task.wait(t)
 	end}
 end
 
-
 local function refreshTimer() ----> Display the new time on client-side
-	pcall(refreshTimer_event.FireAllClients, refreshTimer_event, timer)
+	pcall(refreshTimer_event.FireAllClients, refreshTimer_event, timer//1)
 end
 
 local function roundIntro()
@@ -61,15 +61,15 @@ local function roundIntro()
 	displayMsg("Anyways, we should be fine.", 4).Wait()
 	displayMsg("The military's on their way to extract us.", 4).Wait()
 	displayMsg("Until then, we're under quarantine.", 4).Wait()
-	displayMsg("Stay sharp, and stay alive!", 4).Wait()
+	displayMsg("Stay sharp, and stay alive!", 4, true).Wait()
 end
 
-local function roundEnd()
+local function roundEpilogue()
 	displayMsg("Shortly after these events, the military arrived.", 4).Wait()
 	displayMsg("In the end, the aliens were defeated.", 4).Wait()
 	displayMsg("# residents made it out alive...", 4).Wait()
 	displayMsg("...others are in critical condition, but many are dead.", 4).Wait()
-	displayMsg("Hopefully, this is the first and last we'll time hear of an alien infiltration.", 4).Wait()
+	displayMsg("Hopefully, this is the first and last we'll time hear of an alien infiltration.", 4, true).Wait()
 end
 
 
@@ -83,6 +83,7 @@ end
 -- TP plrs, fire bindable event, fire remote event, start timer
 local function initRound() --> Run once @ start of round
 	round_ongoing = true
+	RM.initPlayers(game.Players:GetPlayers())
 	transitionPeriod()
 	roundIntro()
 end
@@ -91,19 +92,19 @@ end
 local function initIntermission() --> Run once @ start of intermission
 	round_ongoing = false
 	transitionPeriod()
-
 end
 
 
 -----------------------------------------------------------------------
 -- Reset timer, do cleanup
 local function endRound() --> Run once @ end of round
+	RM.stripPlayers()
+	roundEpilogue()
 	timer = 0
 	initIntermission()
 end
 
 local function endIntermission() --> Run once @ end of intermission
-
 	timer = 0
 	initRound()
 end
@@ -111,15 +112,15 @@ end
 
 -----------------------------------------------------------------------
 local function roundPeriodic(dt: number) --> Run every heartbeat while round is ongoing
-	updateTimer()
-
+	if RM.wave_times[timer] then
+		RM.spawnWave(RM.wave_times[timer])
+	end
+	
 	if timer < round_length then return end
 	endRound()
 end
 
 local function intermissionPeriodic(dt: number) --> Run every heartbeat during intermission
-	updateTimer()
-
 	if timer < intermission_length then return end
 	endIntermission()
 end
@@ -139,6 +140,7 @@ end
 -- (This way lets us do transition time w/o breaking anything or doing much modification)
 while task.wait(dt) do
 	timer += dt
+	refreshTimer()
 
 	if round_ongoing then
 		-- Round is ongoing
