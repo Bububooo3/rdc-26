d51615ddb235b7e92da3a3cd3af25558de68c9cf
Richard Scott <bububooo3@gmail.com>
2026-02-07 20:53:21 -0500

    Updated ServerScriptService/Intermission/RoundManager.lua

--- ServerScriptService/Intermission/RoundManager.lua
+++ ServerScriptService/Intermission/RoundManager.lua
@@ -8,6 +8,7 @@ local DS = game:GetService("Debris")
 local manager = {}
 
 local waves_spawned = 0
+local connections: RBXScriptConnection = {}
 
 local enemies = {
 	["A"] = enemy_fldr:WaitForChild("Grunt"),
@@ -33,6 +34,15 @@ local waves = {
 	[6] = {305, {"C", "D"}, 10 , 3}
 }
 
+manager.wave_times = {
+	[5] = 1,
+	[65] = 2,
+	[125] = 3,
+	[185] = 4,
+	[245] = 5,
+	[305] = 6
+}
+
 local plrs_alive: Players = {}
 
 local function distributeEnemyHealth(provider_health: number, hit: boolean) ----> Give leftover health to youngest enemy upon natural death
@@ -41,7 +51,7 @@ end
 
 function manager.spawnEnemy(enemy: string)
 	if not enemies[enemy] then return end
-	
+
 	local rig = enemies[enemy]:Clone()
 	rig.Parent = workspace
 	rig:PivotTo(enemy_spawns[enemy][RNG:NextInteger(1, #enemy_spawns[enemy])].CFrame)
@@ -51,9 +61,53 @@ end
 
 function manager.spawnWave(wave: number)
 	if not waves[wave] then return end
-	
+
 	local wave_data = waves[wave]
 	local spawn_num = wave_data[3] + math.floor(wave_data[4]^(#plrs_alive))
+
+	for i=0, spawn_num, 1 do
+		manager.spawnEnemy(wave_data[2][RNG:NextInteger(1, #wave_data[2])])
+		task.wait(spawn_num//5) ---> Spawning a wave takes around 5 seconds
+	end
+
+	waves_spawned += 1
+end
+
+function manager.reset()
+	waves_spawned = 1
+	table.clear(plrs_alive)
+end
+
+function manager.initPlayers(plrs: {Player})
+	for _, plr in plrs do
+		if not plr.Character then continue end
+
+		local char = plr.Character
+		CS:AddTag(char, "Player")
+		char:SetAttribute("Health", 10)
+		table.insert(plrs_alive, plr)
+
+		table.insert(connections, char.AttributeChanged:Connect(function()
+			if not (char:GetAttribute("Health") <= 0) then return end
+			table.remove(plrs_alive, table.find(plrs_alive, plr))
+			char:WaitForChild("Humanoid").Health = 0
+		end))
+	end
+end
+
+function manager.stripPlayers()
+	for _, conn in connections do
+		conn:Disconnect()
+	end
+	
+	for _, plr in Players:GetPlayers() do
+		if not plr.Character then continue end
+
+		local char = plr.Character
+		CS:RemoveTag(char, "Player")
+		char:SetAttribute("Health", nil)
+		table.insert(plrs_alive, plr)
+	end
 end
 
 return manager
